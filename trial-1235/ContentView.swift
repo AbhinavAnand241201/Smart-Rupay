9e0912067aaadf69cd3b516f039a2bf253736317
AbhinavAnand241201 <iabhinavanand10@gmail.com>
2025-06-02 00:49:42 +0530

    Initial Commit

---
+++ trial-1235/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trial-1235
+//
+//  Created by ABHINAV ANAND  on 02/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trial-1235/trial_1235App.swift
@@ -0,0 +1,17 @@
+//
+//  trial_1235App.swift
+//  trial-1235
+//
+//  Created by ABHINAV ANAND  on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct trial_1235App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
